9691a5dd7fe78bce5f50a0d2707553fabcf9d266
virgiliu guraliuc <virgiliuguraliuc@gmail.com>
2019-08-27 13:46:29 +0300

    Initial commit

---
+++ js/to-do-list.js
@@ -0,0 +1,32 @@
+
+window.ToDoList = {
+    API_BASE_URL:"http://localhost:8083/to-do-items",
+    
+    createItem: function () {
+        var decription = $("#description-field").val();
+        var deadline = $("#deadline-picker").val();
+        
+        
+        var item = {
+            description:decription,
+            deadline:deadline
+        }
+        
+        $.ajax(({
+            url:ToDoList.API_BASE_URL,
+            method: "POST",
+            //MIME type
+            contentType:"application/json",
+            data:JSON.stringify(item)
+        }).done(function (response) {
+            console.log("Succesfully received response")
+            console.log(response);
+        })
+        
+    }
+
+
+
+
+
+}
